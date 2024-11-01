8f91d233dcf22a7ce35c40f5534f21b01711bf57
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2024-11-01 18:23:19 -0400

    Fixing the menubar.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleNavbar() {
   var x = document.getElementById("myTopnav");
+  var icon = document.querySelector(".topnav .icon i");
+
+  // Toggle responsive class and icon
   if (x.className === "topnav") {
-    x.className += " responsive";
+      x.className += " responsive";
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
   } else {
-    x.className = "topnav";
+      x.className = "topnav";
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
   }
 }
